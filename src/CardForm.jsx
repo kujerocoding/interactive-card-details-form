b70efba1b71766b36a183e111d41737a2b9b23c0
herom <jerome.manset22@gmail.com>
2023-02-14 03:57:54 +0800

    add state for form data and a function handling the input change and display the change to the cards

--- src/CardForm.jsx
+++ src/CardForm.jsx
@@ -1,31 +1,78 @@
 import React from 'react'
+import { useState } from 'react'
 
 const CardForm = () => {
+
+  const [formData, setFormData] = useState({
+    cardName: "Jane Appleseed",
+    cardNumber: "1234 5678 9123 0000",
+    expDateMonth: "01",
+    expDateYear: 2001,
+    cvc: 123
+  })
+
+  function handleChange(e) {
+    const {name, value} = e.target
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData, 
+        [name] : value
+      }
+    })
+  }
+
+
+  //console.log(formData)
+
+
   return (
     <>
       <div className='card--container'>
         <div className='center'>
             <div className='card--front'>
                 <img src="./src/assets/images/card-logo.svg" alt="" />
-                <h4>0000 0000 0000 0000</h4>
-                <p>Jane Appleseed<span>00/00</span></p>
+                <h4>{formData.cardNumber}</h4>
+                <p>{formData.cardName}<span>{formData.expDateMonth}/{formData.expDateYear}</span></p>
             </div>
             <div className='card--back'>
-              <p>000</p>
+              <p>{formData.cvc}</p>
             </div>
         </div>
       </div>
       <div className='form--container'>
         <form>
               <label htmlFor="name">CARDHOLDER NAME</label>
-              <input className="card--name" id="name" type="text" placeholder='e.g. Jane Appleseed'/>
+              <input className="card--name"
+              name='cardName'  
+              type="text" 
+              placeholder='e.g. Jane Appleseed'
+              onChange={handleChange}/>
               <label htmlFor="name">CARD NUMBER</label>
-              <input className="card--number"  id="cardNumber" type="number" placeholder='e.g. 1234 5678 9123 0000'/>
+              <input className="card--number"  
+              name="cardNumber" 
+              type="number" 
+              placeholder='e.g. 1234 5678 9123 0000'
+              onChange={handleChange}/>
               <label htmlFor="cvc">EXP. DATE (MM/YY) &emsp;<span>CVC</span></label>
                 <div className='form--lower'>
-                <input className="form--month" id="month" type="number" placeholder='MM' />
-                <input className="form--year" id="year" type="number" placeholder='YY'/>
-                <input className="form--cvc" id="cvc"type="number" placeholder='e.g. 123'/>
+                <input className="form--month" 
+                name="expDateMonth"
+                id="month" 
+                type="number" 
+                placeholder='MM' 
+                onChange={handleChange}/>
+                <input className="form--year" 
+                name="expDateYear"
+                id="year" 
+                type="number" 
+                placeholder='YY'
+                onChange={handleChange}/>
+                <input className="form--cvc" 
+                name="cvc"
+                id="cvc"
+                type="number" 
+                placeholder='e.g. 123'
+                onChange={handleChange}/>
                 </div>  
               <button>Confirm</button>
         </form>
