fd6336d4bb703ed7d675c29c68acd4f3c5380201
herom <jerome.manset22@gmail.com>
2023-02-14 01:19:13 +0800

    add elements

--- src/CardForm.jsx
+++ src/CardForm.jsx
@@ -9,19 +9,22 @@ const CardForm = () => {
 
             </div>
             <div className='card--back'>
-
             </div>
         </div>
       </div>
-      <form >
-            <label htmlFor="name">Name</label>
-            <input id="name" type="text" />
-            <label htmlFor="name">Number</label>
-            <input id="number" type="number" />
-            <label htmlFor="cvc">cvc</label>
-            <input id="cvc"type="number" />
-            <input type="number" min={0} max={99} />
-      </form>
+      <div>
+        <form>
+              <label htmlFor="name">Name</label>
+              <input id="name" type="text" />
+              <label htmlFor="name">Number</label>
+              <input id="cardNumber" type="number" />
+              <label htmlFor="cvc">cvc</label>  
+              <input id="month" type="number" />
+              <input id="year" type="number" />
+              <input id="cvc"type="number" />
+              <button>Confirm</button>
+        </form>
+      </div>
     </>
   )
 }
